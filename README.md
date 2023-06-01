cfeb1e37a59882d8a8a89107bafdeb54fb01206d
Felix Draxler <felix.draxler@iwr.uni-heidelberg.de>
2023-06-01 13:58:31 +0200

    Add licence to loss.py and explain package in README

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# MLAE
-Maximum Likelihood Training of Autoencoders
+# Maximum Likelihood Training of Autoencoders
+
+```bibtex
+@article{sorrenson2023maximum,
+    title = {Maximum Likelihood Training of Autoencoders},
+    author = {Sorrenson, Peter and Draxler, Felix and Rousselot, Armand and Hummerich, Sander and Zimmermann, Lea and Köthe, Ullrich},
+    journal = {arXiv preprint arXiv:2306.XXXXX},
+    year = {2023}
+}
+```
+
+## Basic usage
+
+```python
+
+import torch
+import mlae.loss as loss
+
+
+class AutoEncoder(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = torch.nn.Sequential(...)
+        self.decoder = torch.nn.Sequential(...)
+
+
+model = AutoEncoder()
+optim = ...
+data_loader = ...
+n_epochs = ...
+beta = ...
+
+for epoch in range(n_epochs):
+    for batch in data_loader:
+        optim.zero_grad()
+        loss = loss.mlae_loss(batch, model.encoder, model.decoder, beta)
+        loss.backward()
+        optim.step()
+```
+
+## Installation
+
+### Install via pip
+
+```bash
+git clone https://github.com/vislearn/mlae.git
+cd mlae
+pip install .
+```
+
+Then you can import the package via
+
+```python
+import mlae
+```
+
+### Copy `mlae/loss.py` into your project
+
+If you do not want to add our `mlae` package as a dependency,
+you can also copy the `mlae/loss.py` file into your own project.
+It only depends on `torch`.
+
+## Reproducing the experiments
+
+This section is work in progress. We will add the full code
+to reproduce the experiments in the paper soon.
--- mlae/loss.py
+++ mlae/loss.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) 2023 Computer Vision and Learning Lab, Heidelberg
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from collections import namedtuple
 from math import sqrt
 from typing import Union, Callable
@@ -27,7 +49,7 @@ def sample_v(x: torch.Tensor, hutchinson_samples: int):
 
 
 def ml_surrogate(x: torch.Tensor, encode: Transform, decode: Transform,
-                 hutchinson_samples: int) -> SurrogateOutput:
+                 hutchinson_samples: int = 1) -> SurrogateOutput:
     """
     Compute the per-sample surrogate for the negative log-likelihood and the volume change estimator.
     The gradient of the surrogate is the gradient of the actual negative log-likelihood.
@@ -66,7 +88,8 @@ def ml_surrogate(x: torch.Tensor, encode: Transform, decode: Transform,
 
 def mlae_loss(x: torch.Tensor,
               encode: Transform, decode: Transform,
-              beta: Union[float | torch.Tensor], hutchinson_samples: int) -> torch.Tensor:
+              beta: Union[float | torch.Tensor],
+              hutchinson_samples: int = 1) -> torch.Tensor:
     """
     Compute the per-sample MLAE loss:
     $$
