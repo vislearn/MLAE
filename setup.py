9f181d06f514d4a26e49ae9a70341d72fcc81306
Felix Draxler <felix.draxler@iwr.uni-heidelberg.de>
2023-06-02 13:40:05 +0200

    Add setup.py -> `pip install .` works

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='mlae',
+    description='Maximum Likelihood Autoencoder',
+    version='0.1dev',
+    packages=['mlae'],
+    license='MIT',
+    author='Felix Draxler, Peter Sorrenson',
+    author_email='felix.draxler@iwr.uni-heidelberg.de, peter.sorrenson@iwr.uni-heidelberg.de',
+    url='https://github.com/vislearn/MLAE'
+)
